45d2d32497b12f5227e9c4e9f1fb7ea9adaf540c
Evgeny <153275378+cosackHector@users.noreply.github.com>
2024-10-01 06:57:02 +0300

    refact

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import avatar  from "../../../asets/avatar.jpg";
+import styles from "./User.module.css"
+import avatar  from "../../../assets/avatar.jpg";
 
 const User = ({
     user,
@@ -7,26 +8,18 @@ const User = ({
 }) => {
 
 console.log('user render');
-    const st = {
-        width: '50px',
-        height: '50px',
-        borderRadius: '50%',
-    }
+
     return (
-        <div className="userContainer">
-            <div className="avatar">
-                <img src={avatar} style={st} />
-                <button onClick={()=>onFollow(user.id)}>{user.followed ? 'Отписаться' : 'Подписаться'}</button>
+        <div className={styles.userContainer}>
+            <div className={styles.avatarContainer}>
+                <img className={styles.avatar} src={avatar} />
             </div>
-            <div className="userDescription">
-                <div className="userName">
-                    <h3>{user.name}</h3>
-                    <p>{user.status}</p>
-                </div>
-                <div className="userLocation">
-                    <p>Страна</p>
-                    <p>Город</p>
-                </div>
+            <div className={styles.nameContainer}>
+                <h3 className={styles.name}>{user.name}</h3>
+                <p className={styles.status}>status</p>
+            </div>
+            <div className={styles.btnContainer}>
+                <button className={styles.btn} onClick={()=>onFollow(user.id)}>{user.followed ? '-' : '+'}</button>
             </div>
         </div>
     )
